c55c941b2f08a0bf1a5dbe914d6cc49f791dd278
Jayakrishnan <jayakrishnan.vijayaraghavan@timmons.com>
2016-03-11 10:44:53 -0500

    Added comments

--- CountWordFrequency.py
+++ CountWordFrequency.py
@@ -1,7 +1,10 @@
 import operator
 from operator import itemgetter
 
-
+'''
+Returns a list of tuples containing the top n frequent words in the given string s.
+In case of tie, words are arranged aplhabetically
+'''
 def countFreq(s, n=None):
     words = s.lower().replace(',','').replace('.', ' ').split(' ')
     uniqueWords = list(set(words))
@@ -11,11 +14,11 @@ def countFreq(s, n=None):
         
     L.sort(key=operator.itemgetter(0))
     L.sort(key=operator.itemgetter(1), reverse = True)
-    print L[:n]
+    return L[:n]
 
 def main():
     s = 'betty bought a bit of butter, but the butter was bitter, so she got a better butter, which was better than the bitter butter'
-    countFreq(s, 3)
+    print countFreq(s, 3)
     
 if __name__ == '__main__':
     main()
